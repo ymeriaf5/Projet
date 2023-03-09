0ede2d68a6b4037a1864fcb982eb642e258500ca
ymeriaf5 <younessmeriaf3@gmail.com>
2023-03-09 16:45:04 +0100

    first commit

---
+++ src/main/java/com/example/fin_dev/FinDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.fin_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fin_dev/FinDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fin_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
